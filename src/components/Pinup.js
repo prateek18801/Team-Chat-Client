7e5252ae8dc2da5da3222823f17d4973ac42ac05
Prateek Chaurasia <prateek18801@gmail.com>
2021-11-10 03:47:29 +0530

    added firebase backend to tasks

--- src/components/Pinup.js
+++ src/components/Pinup.js
@@ -1,20 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, addDoc, query, orderBy, where, onSnapshot } from 'firebase/firestore';
+import { auth, db } from '../config/firebase';
+
 import Task from './Task';
+
 import '../assets/Pinup.css';
 
 export default function Pinup() {
 
+    const user = auth.currentUser.displayName;
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [dnt, setDnt] = useState("");
+    const [due, setDue] = useState("");
     const [tasks, setTasks] = useState([]);
     const [display, setDisplay] = useState(true);
 
-    function addTask() {
+    // not-realtime
+    // useEffect(() => {
+    //     async function fetchTasks() {
+    //         const q = query(collection(db, "todos"), orderBy("due"));
+    //         const querySnapshot = await getDocs(q);
+    //         querySnapshot.forEach((doc) => {
+    //             console.log(doc.id);
+    //             console.log(doc.data());
+    //             setTasks(tasks => [...tasks, { id: doc.id, author: doc.data().author, completed: doc.data().completed, created: doc.data().created, description: doc.data().description, due: doc.data().due, modified: doc.data().modified, title: doc.data().title }]);
+    //         })
+    //     }
+    //     fetchTasks();
+    // }, []);
+
+    // realtime
+    useEffect(() => {
+        const q = query(collection(db, "todos"), orderBy("due"));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                const doc = change.doc;
+                if (change.type === "added") {
+                    console.log("New task: ", doc.data());
+                    setTasks(tasks => [...tasks, { id: doc.id, author: doc.data().author, completed: doc.data().completed, created: doc.data().created, description: doc.data().description, due: doc.data().due, modified: doc.data().modified, title: doc.data().title }]);
+                }
+                if (change.type === "modified") {
+                    console.log("Modified task: ", doc.data());
+                }
+                if (change.type === "removed") {
+                    console.log("Removed task: ", doc.data());
+                    console.log(doc.id);
+                    setTasks(tasks => tasks.filter((t)=> t.id !== doc.id));  
+                }
+            });
+        });
+    }, []);
+
+    async function addTask() {
         setDisplay(!display);
         const date = new Date();
-        setDnt(date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + '@' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
-        setTasks([...tasks, { title: title, desc: description, date: dnt }]);
+        const dnt = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + '@' + date.getHours() + ':' + date.getMinutes();
+
+        try {
+            const docRef = await addDoc(collection(db, "todos"), {
+                author: user,
+                completed: false,
+                created: dnt,
+                description: description,
+                due: due,
+                modified: user,
+                title: title
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+        setTitle("");
+        setDescription("");
+        setDue("");
     }
 
     return (
@@ -23,18 +82,18 @@ export default function Pinup() {
 
             <div id="tasks">
                 {tasks.map((task) => {
-                    return (<Task title={task.title} desc={task.desc} date={task.date} />);
+                    return (<Task key={task.id} id={task.id} author={task.author} completed={task.completed} created={task.created} description={task.description} due={task.due} modified={task.modified} title={task.title} />);
                 })}
             </div>
 
             {(display) ?
-                <button className="btn" id="btn-plus" onClick={()=>{setDisplay(!display)}}><span className="material-icons-round">add_box</span></button>
+                <button className="btn" id="btn-plus" onClick={() => { setDisplay(!display) }}><span className="material-icons-round">add_box</span></button>
                 :
                 <form id="add-task" onSubmit={(e) => { e.preventDefault() }}>
-                    <input className="inp inp-title" type="text" placeholder="title" onChange={(e) => { setTitle(e.target.value) }} />
-                    <button className="btn btn-addtask" onClick={addTask}>add task</button>
-                    <input className="inp inp-desc" type="text" placeholder="description" onChange={(e) => { setDescription(e.target.value) }} />
-                    <input className="inp inp-date" type="datetime-local" />
+                    <input className="inp inp-title" type="text" placeholder="title" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+                    <button className="btn btn-addtask" type="button" onClick={addTask}>add task</button>
+                    <input className="inp inp-desc" type="text" placeholder="description" value={description} onChange={(e) => { setDescription(e.target.value) }} />
+                    <input className="inp inp-date" type="datetime-local" value={due} onChange={(e) => { setDue(e.target.value) }} />
                 </form>
             }
         </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,52 @@
+import { useState } from 'react';
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from '../config/firebase';
+
 import '../assets/Task.css';
 
-export default function Task({title, desc, date}) {
+export default function Task({ id, author, completed, created, description, due, modified, title }) {
+    
+    const [rotate, setRotate] = useState("material-icons-round rotate");
+    const [display, setDisplay] = useState("task-desc display-none");
+    const [status, setStatus] = useState("pending_actions");
+
+
+    function toggleExpand(){
+        if(rotate === "material-icons-round"){
+            setRotate("material-icons-round rotate");
+            setDisplay("task-desc display-none");
+        }else{
+            setRotate("material-icons-round");
+            setDisplay("task-desc");
+        }
+    }
+
+    function toggleStatus(){
+        (status === "task_alt") ?
+            setStatus("pending_actions")
+            :
+            setStatus("task_alt");
+    }
+
+    async function deleteTask(){
+        await deleteDoc(doc(db, "todos", id));
+    }
+
     return (
         <div id="task">
-            <h3 className="task-title">{title}</h3>
-            <div className="task-desc">{desc}</div>
+            <div className="task-header">
+                <button className="btn expand-task" onClick={toggleExpand}><span className={rotate}>expand_more</span></button>
+                <div className="task-status"><span className="material-icons-round">{status}</span></div> 
+                <h3 className="task-title">{title}</h3>
+                <button className="btn check-task" onClick={toggleStatus}><span className="material-icons-round">check</span></button>
+                <button className="btn delete-task" onClick={deleteTask}><span className="material-icons-round">delete_outline</span></button>
+            </div>
+            <div className={display}>{description}</div>
             <div className="task-info">
-                <div className="time">{date}</div>
+                <div className="time">Date Created: {created}</div>
+                <div className="time">Deadline: {due}</div>
+                <div className="created-task">Created By: {author}</div>
+                <div className="modified-task">Last Modified By: {modified}</div>
             </div>
         </div>
     )
